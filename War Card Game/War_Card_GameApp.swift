1348a9d5242729fa89bd547de19bb673dece12af
Gabriel <gabrielandre699@gmail.com>
2023-07-10 19:32:00 -0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Gabriel André Barreto Olimpio dos Santos on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
